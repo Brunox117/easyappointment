837031a598b96216182122597a0bb90e2926b9c1
Bruno Jimenez <omarjm0910@gmail.com>
2026-01-01 16:21:52 -0600

    Agregamos el modulo de autenticacion y usuarios hay que decidir si eliminamos doctores y movemos todo a users

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { GetUser } from './decorators/get-user.decoratos';
+import { User } from './entities/user.entity';
+import { Auth } from './decorators/auth.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Get('check-auth-status')
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+      await this.userRepository.save(user);
+      const { password: _, ...userToReturn } = user;
+      return {
+        ...userToReturn,
+        token: this.getJwtToken({ id: user.id }),
+      };
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  checkAuthStatus(user: User) {
+    const { email, fullName } = user;
+    return {
+      email,
+      fullName,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true, id: true },
+    });
+    if (!user) throw new UnauthorizedException('Not valid credentials');
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Not valid credentials');
+    return { ...user, token: this.getJwtToken({ id: user.id }) };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    throw new InternalServerErrorException(
+      'Unknown error please, contact the server admin',
+    );
+  }
+}
